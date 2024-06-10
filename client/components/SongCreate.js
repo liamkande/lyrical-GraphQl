121831a57eef19ba1803cf5ca7baf9a9677c5986
Liam Ansoumane Kande <liamkande@icloud.com>
2024-06-09 19:40:47 -0700

    adds SongCreate input

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SongCreate = (props) => {
-
-    return (
-        <div>
-            <h3>Create a New Song</h3>
-        </div>
-    );
+class SongCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: "" };
+    }
+    render() {
+        return (
+            <div>
+                <h3>Create a New Song</h3>
+                <form>
+                    <label>Song Title:</label>
+                    <input
+                        onChange={event => this.setState({ title: event.target.value })}
+                        value={this.state.title}
+                    />
+                </form>
+            </div>
+        );
+    }
 }
 export default SongCreate;
