0f76b84fae7455bdc05dc9d022157557817196b0
Liam Ansoumane Kande <liamkande@icloud.com>
2024-06-09 18:18:34 -0700

    configured routing

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,12 +3,25 @@ import ggl from "graphql-tag";
 import {graphql} from "react-apollo";
 
 class SongList extends React.Component {
+
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            );
+        });
+    }
+
     render() {
-        console.log(this.props)
+        if (!this.props.data.songs) {
+            return <div>Loading...</div>;
+        }
         return (
-            <div>
-                SongList
-            </div>
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
         );
     }
 }
@@ -18,6 +31,7 @@ console.log(SongList)
 const query = ggl`
     {
         songs {
+            id
             title
         }
     }
--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-client";
 import {ApolloProvider} from "react-apollo";
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import App  from "./components/App";
 import SongList from "./components/SongList";
 
 const client = new ApolloClient({});
@@ -9,7 +11,11 @@ const client = new ApolloClient({});
 const Root = () => {
     return (
         <ApolloProvider client={client}>
-            <SongList/>
+               <Router history={hashHistory}>
+                    <Route path="/" component={App}>
+                        <IndexRoute component={SongList}/>
+                    </Route>
+                </Router>
         </ApolloProvider>
     )
 };
