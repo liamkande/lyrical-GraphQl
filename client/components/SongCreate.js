ac6b635d6782d979b7d973152e6b9cfd52e55a0a
Liam Ansoumane Kande <liamkande@icloud.com>
2024-06-09 19:57:59 -0700

    adds SongCreate mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import ggl from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class SongCreate extends Component {
     constructor(props) {
         super(props);
         this.state = { title: "" };
     }
+
+    onsubmit(e) {
+        e.preventDefault();
+        this.props.mutate({
+            variables: { title: this.state.title }
+        });
+        this.setState({ title: "" });
+    }
     render() {
         return (
             <div>
                 <h3>Create a New Song</h3>
-                <form>
+                <form onSubmit={this.onsubmit.bind(this)}>
                     <label>Song Title:</label>
                     <input
                         onChange={event => this.setState({ title: event.target.value })}
@@ -20,4 +30,12 @@ class SongCreate extends Component {
         );
     }
 }
-export default SongCreate;
+const mutation = ggl`
+    mutation AddSong($title: String) {
+        addSong(title: $title) {
+            title
+        }
+    }
+`;
+
+export default graphql(mutation) (SongCreate);
