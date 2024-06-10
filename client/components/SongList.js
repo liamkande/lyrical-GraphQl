65fcb9fbd34053875dab8b38002d0520cf3a0559
Liam Ansoumane Kande <liamkande@icloud.com>
2024-06-09 20:53:16 -0700

    adds delete mutation to SongList

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,39 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import ggl from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
 import query from "../queries/fetchSongs";
 
-const SongList = (props) => {
-    const { data } = props;
+class SongList extends Component {
+    constructor(props) {
+        super(props);
 
-    const renderSongs = () => {
-        return data.songs.map(song => {
+        this.onSongDelete = this.onSongDelete.bind(this);
+        this.renderSongs = this.renderSongs.bind(this);
+    }
+
+    onSongDelete(id) {
+        this.props
+            .mutate({ variables: { id } })
+            .then(() => this.props.data.refetch());
+    }
+
+    renderSongs() {
+        const { data } = this.props;
+        if (!data.songs) return null;
+
+        return data.songs.map(({ id, title }) => {
             return (
-                <li key={song.id} className="collection-item">
-                    {song.title}
+                <li key={id} className="collection-item">
+                    {title}
+                    <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+                        delete
+                    </i>
                 </li>
             );
         });
-    };
-
-    if (!data.songs) {
-        return <div>Loading...</div>;
     }
 
-    return (
-        <div>
-            <ul className="collection">
-                {renderSongs()}
-            </ul>
-            <Link
-                onlyActiveOnIndex={true}
-                to="/songs/new"
-                className="btn-floating btn-large red right"
-            >
-            <i className="material-icons">add</i>
-            </Link>
-        </div>
-    );
-};
+    render() {
+        return (
+            <div>
+                <ul className="collection">{this.renderSongs()}</ul>
+                <Link
+                    onlyActiveOnIndex={true}
+                    to="/songs/new"
+                    className="btn-floating btn-large red right"
+                >
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
+        );
+    }
+}
 
-export default graphql(query)(SongList);
+const mutation = ggl`
+  mutation DeleteSong($id: ID!) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(graphql(query)(SongList));
